1fa49ed4d3508083a4fdd202e798ef4fe6abfae4
Asif Ullah Sazzad <asifullahsazzad0@gmail.com>
2026-04-26 12:56:02 +0600

    add untracked files

--- src/components/AdsComponent.jsx
+++ src/components/AdsComponent.jsx
@@ -2,10 +2,12 @@ import adsImage from '../assets/bg.png'
 
 const AdsComponent = () => {
     return (
-        <div className='w-full bg-center bg-cover text-white h-120 flex flex-col justify-center text-center items-center space-y-5' style={{backgroundImage: `url(${adsImage})`}}>
-            <h1 className='font-bold text-3xl'>Create an Amazing Newspaper</h1>
+        <div className='w-full bg-center bg-cover text-white h-120 flex flex-col justify-center text-center items-center' style={{backgroundImage: `url(${adsImage})`}}>
+           <div className='w-2/3 space-y-5'>
+             <h1 className='font-bold text-3xl'>Create an Amazing Newspaper</h1>
             <p className='text-base'>Discover thousands of options, easy to customize layouts, one-click to import demo and much more.</p>
             <button className='text-white btn bg-[#D72050] border-none shadow-none'>Learn More</button>
+           </div>
         </div>
     );
 };
